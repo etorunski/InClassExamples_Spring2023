19b47ece2666b72b6e3e131dec3bce0df7c96c07
etorunski <etorunski@gmail.com>
2023-05-16 15:25:49 -0400

    added 8 lines

--- app/src/main/java/com/example/ericsandroidlabs/MainActivity.java
+++ app/src/main/java/com/example/ericsandroidlabs/MainActivity.java
@@ -15,4 +15,13 @@ public class MainActivity extends AppCompatActivity {
         //loads an XML file on the page
         setContentView(  R.layout.activity_main   );
     }
+
+
+//add 8 blank spaces
+
+
+
+
+
+
 }
