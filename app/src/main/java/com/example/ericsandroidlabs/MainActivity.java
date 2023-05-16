d4fee368287497a64fa719036aca896f5b33a9fd
etorunski <etorunski@gmail.com>
2023-05-16 16:03:36 -0400

    showed text widgets

--- app/src/main/java/com/example/ericsandroidlabs/MainActivity.java
+++ app/src/main/java/com/example/ericsandroidlabs/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.ericsandroidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +19,30 @@ public class MainActivity extends AppCompatActivity {
 
         //loads an XML file on the page
         setContentView(  R.layout.activity_main   );
+
+
+        //look for something with the id "theText", return that object
+        TextView theText = findViewById( R.id.theText ); //same as getElementById in Javascript
+
+
+        Button b = findViewById(R.id.theButton); //will search XML for something with id theButton
+        EditText theEdit = findViewById( R.id.theEditText );
+
+
+                            //anonymous class
+    b.setOnClickListener(new View.OnClickListener() {
+        //Provide the missing function:
+        @Override
+        public void onClick(View v) {
+
+            String words = theEdit.getText().toString(); // return what's in the EditText
+            //change what is in the textView;
+            theText.setText(words);
+        }
+    });
+
+
+
     }
 
 }
