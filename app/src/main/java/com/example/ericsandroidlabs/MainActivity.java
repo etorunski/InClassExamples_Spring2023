3df91663aafe731fe1f7860a8c3610b2a4620123
Eric Torunski <etorunski@gmail.com>
2023-07-18 16:32:59 -0400

    finished week 10 server query

--- app/src/main/java/com/example/ericsandroidlabs/MainActivity.java
+++ app/src/main/java/com/example/ericsandroidlabs/MainActivity.java
@@ -1,10 +1,24 @@
 package com.example.ericsandroidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.ericsandroidlabs.databinding.ActivityMainBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URLEncoder;
 
 
 /**This page represents the first page loaded
@@ -15,63 +29,78 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
-    /** This is a javadoc comment */
+    // for sending network requests:
+    RequestQueue queue = null;
 
-    /*   This is a normal comment */
-
-    /** This holds the "Hello world" text view */
-    protected TextView theText;
-
-
-    /** This holds the "Click me" button */
-    protected Button myButton;
-
-
-    /** This holds the edit text for typing into */
-    protected EditText theEditText;
 
     //equivalent to        static void main(String args[])
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); //calling onCreate from parent class
 
+        ActivityMainBinding binding = ActivityMainBinding.inflate( getLayoutInflater() );
+
+        //This part goes at the top of the onCreate function:
+        queue = Volley.newRequestQueue(this); //like a constructor
 
         //loads an XML file on the page
-        setContentView(  R.layout.activity_main   );
+        setContentView(  binding.getRoot()  );
 
-        theText = findViewById(R.id.textView);
-        myButton = findViewById(R.id.button);
-        theEditText = findViewById(R.id.theEditText);
 
-        myButton.setOnClickListener( click -> {
-            String input = theEditText.getText().toString();
+        binding.button.setOnClickListener( click -> {
 
-            if(checkInput(input, '='))
-            {
-                theText.setText("We found =");
-            }
-            else theText.setText("NO = found in the text");
+            String cityName = binding.theEditText.getText().toString();
+
+            //server name and parameters:                           name=value&name2=value2&name3=value3
+            String url = "https://api.openweathermap.org/data/2.5/weather?q=" +
+                    URLEncoder.encode(cityName) //replace spaces, &. = with other characters
+                    + "&appid=7e943c97096a9784391a981c4d878b22&units=metric";
+
+            JsonObjectRequest request =   new JsonObjectRequest(Request.Method.GET, url, null,
+                    ( successfulResponse ) -> {
+
+                        JSONObject main = null;
+                        try {
+                            main = successfulResponse.getJSONObject( "main"  );
+                            double temp = main.getDouble( "temp" );
+
+                            binding.textView.setText("The temperature is " + temp + " degrees");
+
+                            JSONArray weatherArray = successfulResponse.getJSONArray( "weather");
+
+                            JSONObject pos0 = weatherArray.getJSONObject( 0 );
+                            String  iconName = pos0.getString("icon");
+
+                            String pictureURL = "http://openweathermap.org/img/w/" + iconName + ".png";
+
+                            ImageRequest imgReq = new ImageRequest(pictureURL, new Response.Listener<Bitmap>() {
+                                @Override
+                                public void onResponse(Bitmap bitmap) {
+
+                                    int i = 0;
+                                }
+                            }, 1024, 1024, ImageView.ScaleType.CENTER, null,
+                                    (error ) -> {
+                                    int i = 0;
+                            });
+
+                            queue.add(imgReq);
+
+                        }
+                        catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+
+
+                    }, //gets called if it is successful
+                    (vError) ->   {
+                            int i = 0;
+
+                    }  ); //gets called if there is an error
+
+            queue.add(request); //run the web query
 
         });
     }
-
-    /** This function checks the input string
-     *
-     * @param input  The string to search
-     * @param toLookFOr  The character to compare
-     * @return true if toLookFor is part of the input string, false otherwise
-     */
-
-    boolean checkInput(String input, char toLookFOr  ){
-        boolean found = false;
-
-        for(int i = 0; i < input.length(); i++ ){
-            char c = input.charAt(i);
-            if(c == toLookFOr)
-                found = true;
-        }
-        return found;
-    }
-
 
 }
