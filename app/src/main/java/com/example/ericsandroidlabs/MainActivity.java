c6a0ffea5f2a8215e21329dbd779ae7efe7d020c
Eric Torunski <etorunski@gmail.com>
2023-07-25 15:53:25 -0400

    finihsed navigation drawer

--- app/src/main/java/com/example/ericsandroidlabs/MainActivity.java
+++ app/src/main/java/com/example/ericsandroidlabs/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.ericsandroidlabs;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
@@ -11,6 +14,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -36,21 +41,6 @@ import java.util.concurrent.Executors;
 public class MainActivity extends AppCompatActivity {
 
 
-    protected ArrayList<ChatMessage> theWords;
-
-
-    /** This holds the "Click me" button */
-    protected Button myButton;
-    protected RecyclerView recyclerView;
-
-    MessageDatabase myDB ;
-    ChatMessageDAO myDAO;
-
-    /** This holds the edit text for typing into */
-    protected EditText theEditText;
-    MainActivityViewModel model;
-    RecyclerView.Adapter myAdapter;
-    //equivalent to        static void main(String args[])
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); //calling onCreate from parent class
@@ -60,185 +50,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(  binding.getRoot()  );
 
 
-        FrameLayout fragmentLocation = findViewById( R.id.fragmentLocation);
+        setSupportActionBar(binding.myToolbar); //adds the toolbar
 
-        boolean IAmTablet = fragmentLocation != null;
+        DrawerLayout drawer = binding.drawerLayout;
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, binding.myToolbar,
+                R.string.open, R.string.close);
 
+        drawer.addDrawerListener( toggle ); //rotate the button when opening
 
-        model = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        toggle.syncState(); //initialized the button
 
-        model.chosenMessage.observe(this, newChatMessage -> {
+        binding.myNavigationView.setNavigationItemSelectedListener(
+                (menuItem) -> {
 
-            if(newChatMessage != null) {
-                FragmentManager fMgr = getSupportFragmentManager();
+                    if(menuItem.getItemId() == R.id.garbage_id)
+                    {
+                        Log.d("CLicked", "You clicked the gargbage pail");
+                    }
+                    else if(menuItem.getItemId() == R.id.garbage_2)
+                    {
+                        Log.d("CLicked", "You clicked the gargbage 2");
+                    }
 
-                FragmentTransaction tx = fMgr.beginTransaction();
-
-                //what to show:
-                DetailsFragment frag = new DetailsFragment( newChatMessage );
-
-                //where to load:
-                tx.add(R.id.fragmentLocation, frag);
-                tx.addToBackStack("aqnything here");
-                tx.commit();//this will show it
-            }
-        });
-
-        myDB = Room.databaseBuilder(getApplicationContext(), MessageDatabase.class, "database-name").build();
-
-        myDAO = myDB.cmDAO();//the only function in MessageDatabase;
-
-//all new messages:
-        theWords = model.theWords;
-
-
-        Executor thread = Executors.newSingleThreadExecutor();
-        //add all previous messages from database:
-        thread.execute(() -> {
-                List<ChatMessage> allMessages = myDAO.getAllMessages();
-                theWords.addAll(allMessages);
-          });
-
-
-        myButton = binding.button;
-        theEditText = binding.theEditText;
-        recyclerView = binding.theRecycleView;
-
-        myButton.setOnClickListener( click -> {
-            String input = theEditText.getText().toString();
-
-            int type = 1;
-            SimpleDateFormat sdf = new SimpleDateFormat("EEEE, dd-MMM-yyyy hh-mm-ss a");
-            String currentDateandTime = sdf.format(new Date());
-
-            ChatMessage newMessage = new ChatMessage(input, currentDateandTime, type);
-            Executor thread1 = Executors.newSingleThreadExecutor();
-            thread1.execute(() ->{
-                    newMessage.id = myDAO.anyFunctionNameForInsertion(newMessage);//add to database;
-                   /*this runs in another thread*/
-            });
-
-            //insert into ArrayList
-            theWords.add(newMessage);
-
-            myAdapter.notifyDataSetChanged(); //updates the rows
-
-            theEditText.setText("");
-        });
-
-        //for recycler view:
-
-
-        recyclerView.setAdapter(myAdapter = new RecyclerView.Adapter<MyRowHolder>() {
-            @NonNull
-            @Override
-            public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-               //this inflates the row layout
-
-                //int viewType is what layout to load
-                if(viewType == 1) {
-                    SentRowLayoutBinding binding =                           //how big is parent?
-                            SentRowLayoutBinding.inflate(getLayoutInflater(), parent, false);
-
-                       return new MyRowHolder(binding.getRoot());
+                    drawer.closeDrawer(GravityCompat.START); //close to the left
+                    return true;
                 }
-                else {
-                    ReceiveRowLayoutBinding binding =
-                            ReceiveRowLayoutBinding.inflate(getLayoutInflater(), parent, false);
-                    return new MyRowHolder(binding.getRoot());
-                }
-            }
-            public int getItemViewType(int position){
 
-                //even
-                if (position % 2 == 0)
-                    return 1;
-                else //odd
-                    return 2;
-            }
-            @Override
-            public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
-
-                //update the widgets:
-                ChatMessage atThisRow = theWords.get(position);
-                holder.theTime.setText(atThisRow.timeSent);
-                holder.theWord.setText(  atThisRow.message );//puts the string in position at theWord TextView
-            }
-
-            @Override
-            public int getItemCount() {
-                //how many rows there are:
-                return theWords.size();
-            }
-        });
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        ); //onOptionItemSelected
     }
 
-    protected class MyRowHolder extends RecyclerView.ViewHolder {
-        // put variables for what is on a single row:
-        TextView theWord;
-        TextView theTime;
-                                    //This is a row:
-        public MyRowHolder(@NonNull View itemView) {
-            super(itemView);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         super.onCreateOptionsMenu(menu);
 
-            itemView.setOnClickListener( click -> {
-
-                int index = getAbsoluteAdapterPosition();
-                ChatMessage thisMessage = theWords.get(index);
-                model.chosenMessage.postValue(  thisMessage );
-
-                /*
-                AlertDialog.Builder builder = new AlertDialog.Builder( MainActivity.this );
-                builder.setMessage("Do you want to delete this?")
-                        .setTitle("QUestion")
-                        .setPositiveButton("Go Ahead",(dlg, which)->{
-
-                    //what is the index?
-                    int index = getAbsoluteAdapterPosition();
-
-                    ChatMessage toDelete = theWords.get(index);
-
-                    Executor thread1 = Executors.newSingleThreadExecutor();
-                    thread1.execute(() ->{
-                        myDAO.deleteThisChatMessage( toDelete );
-                        theWords.remove(index);//remove from our array list
-
-                        //must be done on the main UI thread
-                        runOnUiThread(() -> {  myAdapter.notifyDataSetChanged(); });
-
-                        Snackbar.make( recyclerView, "Deleted your message", Snackbar.LENGTH_LONG)
-                                .setAction("UNDO", clk -> {
-                                    Executor myTHread = Executors.newSingleThreadExecutor();
-                                    myTHread.execute(() -> {
-                                        myDAO.anyFunctionNameForInsertion(toDelete);
-
-                                        theWords.add(index,toDelete );
-                                       runOnUiThread( () ->  myAdapter.notifyDataSetChanged());
-                                    });
-
-
-
-                                } )
-                                .show();
-                        /*this runs in another thread* /
-                    });
-
-
-                } )
-                        .setNegativeButton("No", (dl, wh)->{ /*Hide the dialog, do nothing* /})
-
-                //appear:
-                .create().show();*/
-            }
-
-            );
-            //THis holds the message Text:
-            theWord = itemView.findViewById(R.id.theMessage);
-
-            //This holds the time text
-            theTime = itemView.findViewById(R.id.theTime);
-        }
+         getMenuInflater().inflate(   R.menu.my_menu,  menu);
+         return true;
     }
 }
--- app/src/main/java/com/example/ericsandroidlabs/data/MainActivityViewModel.java
+++ app/src/main/java/com/example/ericsandroidlabs/data/MainActivityViewModel.java
@@ -4,11 +4,7 @@ package com.example.ericsandroidlabs.data;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.example.ericsandroidlabs.ChatMessage;
-
 import java.util.ArrayList;
 
 public class MainActivityViewModel extends ViewModel {
-    public ArrayList<ChatMessage> theWords = new ArrayList<>();
-    public MutableLiveData<ChatMessage> chosenMessage = new MutableLiveData<ChatMessage>();
-}
+ }
